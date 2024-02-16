fb1bf793a2195c545852534144bef673d965c00d
Willian Vassilek <willz3@outlook.com>
2024-02-16 16:48:59 -0300

    test: should return an error if the user does not exists.

--- src/test/java/com/example/cleanarch/data/usecase/GetUserByIdUseCaseTest.java
+++ src/test/java/com/example/cleanarch/data/usecase/GetUserByIdUseCaseTest.java
@@ -3,6 +3,7 @@ package com.example.cleanarch.data.usecase;
 import com.example.cleanarch.core.shared.logic.Either;
 import com.example.cleanarch.data.protocols.gateway.user.IGetUserByIdGateway;
 import com.example.cleanarch.domain.entity.UserEntity;
+import com.example.cleanarch.domain.exception.UserNotFoundError;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,6 +39,19 @@ class GetUserByIdUseCaseTest {
 
         assertTrue(result.isRight());
         assertEquals(result.getRight(), Optional.of(userEntity));
+    }
+
+    @Test
+    @DisplayName("should return an error if user does not exists.")
+    void getErrorWhenUserDoesNotExists() {
+        UserEntity userEntity = makeUserEntity();
+        when(userGateway.findById(userEntity.getId())).thenReturn(null);
+
+        Either<Error, UserEntity> result = sut.execute(userEntity.getId());
+
+        assertTrue(result.isLeft());
+        assertTrue(result.getLeft().isPresent());
+        assertInstanceOf(UserNotFoundError.class, result.getLeft().get());
     }
 
     UserEntity makeUserEntity() {
