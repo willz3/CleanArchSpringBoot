9071909376e58c7e4338d4e1b5b519fed3baacd6
Willian <willz3@outlook.com>
2024-02-16 17:55:46 -0300

    test: should return 200 if the creation of user succeeds.

---
+++ src/test/java/com/example/cleanarch/infrastructor/controller/user/v1/CreateUserControllerTest.java
@@ -0,0 +1,65 @@
+package com.example.cleanarch.infrastructor.controller.user.v1;
+
+import com.example.cleanarch.core.shared.logic.Either;
+import com.example.cleanarch.domain.entity.UserEntity;
+import com.example.cleanarch.domain.usecase.ICreateUserUseCase;
+import com.example.cleanarch.infrastructor.controller.user.v1.mapper.CreateUserDTOMapper;
+import com.example.cleanarch.infrastructor.controller.user.v1.request.CreateUserRequest;
+import com.example.cleanarch.infrastructor.controller.user.v1.response.CreateUserResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class CreateUserControllerTest {
+
+    @Mock
+    private ICreateUserUseCase createUserUseCase;
+
+    @Mock
+    private CreateUserDTOMapper createUserDTOMapper;
+
+    @InjectMocks
+    private CreateUserController sut;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("should return 200 if the creation of user succeeds.")
+    void returnOk() {
+        UserEntity userEntity = makeUserEntity(null);
+        UserEntity userEntityWithId = makeUserEntity(1L);
+        CreateUserRequest request = makeRequest();
+        CreateUserResponse response = makeResponse();
+
+        when(createUserDTOMapper.toUser(request)).thenReturn(userEntity);
+        when(createUserUseCase.execute(userEntity)).thenReturn(Either.Right(userEntityWithId));
+        when(createUserDTOMapper.toResponse(userEntityWithId)).thenReturn(response);
+
+        ResponseEntity<Object> result = sut.handle(request);
+        assertEquals(result.getStatusCode().value(), 200);
+        assertEquals(result.getBody(), response);
+    }
+
+    UserEntity makeUserEntity(Long id) {
+        return new UserEntity(id, "any_user_name", "password", "email@example.com");
+    }
+
+    CreateUserRequest makeRequest() {
+        return new CreateUserRequest("any_user_name", "password", "email@example.com");
+    }
+
+    CreateUserResponse makeResponse() {
+        return new CreateUserResponse(1L, "any_user_name", "email@example.com");
+    }
+
+}
