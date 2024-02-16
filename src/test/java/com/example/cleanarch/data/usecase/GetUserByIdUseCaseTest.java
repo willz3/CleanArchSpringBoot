6e5736f1b1eb7b19a4c3c8fd2b187bc52bd445c5
Willian Vassilek <willz3@outlook.com>
2024-02-16 16:46:20 -0300

    test: should return an user if it exists.

---
+++ src/test/java/com/example/cleanarch/data/usecase/GetUserByIdUseCaseTest.java
@@ -0,0 +1,47 @@
+package com.example.cleanarch.data.usecase;
+
+import com.example.cleanarch.core.shared.logic.Either;
+import com.example.cleanarch.data.protocols.gateway.user.IGetUserByIdGateway;
+import com.example.cleanarch.domain.entity.UserEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class GetUserByIdUseCaseTest {
+
+    @Mock
+    private IGetUserByIdGateway userGateway;
+
+    @InjectMocks
+    private GetUserByIdUseCase sut;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("should return an user if it exists.")
+    void getUserWhenUserExists() {
+        UserEntity userEntity = makeUserEntity();
+        when(userGateway.findById(userEntity.getId())).thenReturn(userEntity);
+
+        Either<Error, UserEntity> result = sut.execute(userEntity.getId());
+
+        assertTrue(result.isRight());
+        assertEquals(result.getRight(), Optional.of(userEntity));
+    }
+
+    UserEntity makeUserEntity() {
+        return new UserEntity(1L, "any_user_name", "password", "email@example.com");
+    }
+
+}
